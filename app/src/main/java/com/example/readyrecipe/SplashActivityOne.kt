a69206284056e2a53f344a2f98d1ff2fe5649e07
Admin1 <nyashnybastard@gmail.com>
2022-07-07 17:26:08 +0500

    //add_splashActivity

---
+++ app/src/main/java/com/example/readyrecipe/SplashActivityOne.kt
@@ -0,0 +1,40 @@
+package com.example.readyrecipe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivityOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        makeFullScreen()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        setContentView(R.layout.activity_splash_one)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 3000)
+    }
+
+    private fun makeFullScreen(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+
+        )
+        supportActionBar?.hide()
+
+
+
+
+    }
+}
