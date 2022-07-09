4dfa4d479a66ac524cb23614aca3b050822945f3
Admin1 <nyashnybastard@gmail.com>
2022-07-09 17:34:17 +0500

    //add_menu

--- app/src/main/java/com/example/readyrecipe/MainActivity.kt
+++ app/src/main/java/com/example/readyrecipe/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.readyrecipe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-
         setContentView(R.layout.activity_main)
 
 
     }
+
 
 }
