3d98e597846326c50a218a9c9664509a832ba378
Admin1 <nyashnybastard@gmail.com>
2022-07-22 15:01:53 +0500

    //add

--- app/src/main/java/com/example/readyrecipe/MainActivity.kt
+++ app/src/main/java/com/example/readyrecipe/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.readyrecipe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        window.navigationBarColor = resources.getColor(R.color.d_grey)
 
     }
+
 
 
 }
