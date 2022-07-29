aaa1bde88c7e7f5078c5e63db3e68c5103e11a0b
Admin1 <nyashnybastard@gmail.com>
2022-07-29 15:44:21 +0500

    //add_fragments

--- app/src/main/java/com/example/readyrecipe/MainActivity.kt
+++ app/src/main/java/com/example/readyrecipe/MainActivity.kt
@@ -6,14 +6,52 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.example.readyrecipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(Fragment_home())
+        binding.bottomBar.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.mHome -> replaceFragment(Fragment_home())
+                R.id.mNote -> replaceFragment(Fragment_recipe())
+                R.id.mFridge -> replaceFragment(Fragment_fridge())
+                R.id.mSetting -> replaceFragment(Fragment_setting())
+
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+
+
+
+
         //меняю цвет нижнего бара
         window.navigationBarColor = resources.getColor(R.color.white)
+
+
+    }
+
+    //основной метод для смены фрагментов в контейнере fragment_main
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_main,fragment)
+        fragmentTransaction.commit()
 
     }
 
