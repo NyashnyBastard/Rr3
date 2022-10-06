4f10ec3b6efd691f6da3d63e49fbd2982ae6008e
NyashnyBastard <69503765+NyashnyBastard@users.noreply.github.com>
2022-10-06 11:20:43 +0300

    //add new recipe

--- app/src/main/java/com/example/readyrecipe/NewRecipeFragment.kt
+++ app/src/main/java/com/example/readyrecipe/NewRecipeFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_tablayout_categor.newInstance] factory method to
+ * Use the [NewRecipeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_tablayout_categor : Fragment() {
+class NewRecipeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class fragment_tablayout_categor : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tablayout_categor, container, false)
+        return inflater.inflate(R.layout.fragment_new_recipe, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class fragment_tablayout_categor : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_tablayout_categor.
+         * @return A new instance of fragment NewRecipeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            fragment_tablayout_categor().apply {
+            NewRecipeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
